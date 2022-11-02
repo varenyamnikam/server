1d3bb7abffc101c6dc2275e37527f7a9b1310446
varenyam <varenyamnikam004@gmail.com>
2022-11-02 20:21:15 +0530

    for digitalOcean

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
 );
 
 if ((process.env.NODE_ENV = "production")) {
-  app.use(express.static("client/build"));
+  app.use(express.static("static"));
 }
 // app.get("*", function (req, res) {
 //   res.sendFile(path.join(__dirname, "../public", "index.html"));
